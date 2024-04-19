01a58a635785baca9794e4840eb2d5b6e339a8f8
Acnchan8 <acnchan8@gmail.com>
2024-04-18 18:09:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/couplesync/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.couplesync
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.codepath.asynchttpclient.RequestHeaders
+import okhttp3.Headers
+
+
+class MainActivity : AppCompatActivity() {
+    var restaurantImageURL = ""
+    var restaurantName = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val restaurantImageView = findViewById<ImageView>(R.id.restaurantImage)
+        val nameTextView = findViewById<TextView>(R.id.restaurantName)
+
+        getRandomRestaurant(restaurantImageView, nameTextView)
+    }
+
+    private fun getRandomRestaurant(imageView: ImageView, nameTextView: TextView) {
+        val client = AsyncHttpClient()
+        val apiKey = "H1XoYkdmEbtFuydLRM9D8A0cubbOJAqKL2K3KoGxIzgTKqptu_DYyefGJO_OGEh5Hso3SQZMnoxlSti-nP7zuOEuCK5DCLoMVtK-jV6VXiPj51euyN4knvbC9XIhZnYx"
+        val url = "https://api.yelp.com/v3/businesses/search?location=NewYork&limit=50"
+
+        val headers = RequestHeaders()
+        headers.set("Authorization", "Bearer $apiKey")
+        client.get(url, headers, null, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Yelp", "API Request Success")
+                val businesses = json.jsonObject.getJSONArray("businesses")
+                val randomIndex = (0 until businesses.length()).random()
+                val randomRestaurant = businesses.getJSONObject(randomIndex)
+
+                val name = randomRestaurant.getString("name")
+                val imageUrl = randomRestaurant.getString("image_url")
+
+                Glide.with(this@MainActivity).load(imageUrl).into(imageView)
+                nameTextView.text = name
+            }
+            override fun onFailure(statusCode: Int, headers: Headers?, errorResponse: String, throwable: Throwable?) {
+                Log.d("Yelp Error", "API Request Failed: $errorResponse")
+            }
+        })
+    }
+}
